28d580564e65da584367c96666dd83bf42d7d7e8
Siarhei <bordov9635@gmail.com>
2023-04-28 13:57:26 +0300

    commit1

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character == 'q' ||  character == 'w' ||  character == 'e' ||  character == 'r' ||  character == 't' ||  character == 'y' ||  character == 'u' || character == 'o' ||  character == 'b' ){
+            System.out.println("Vowel");
+        } else if (character == 'Q' || character == 'W' || character == 'E' || character == 'R' || character == 'T' || character == 'Y' || character == 'U' || character == 'O' || character == 'B') {
+            System.out.println("Consonant");
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if (symbol == 'q' || symbol == 'Q' || symbol == 'w' || symbol == 'W' || symbol == 'e' || symbol == 'E' || symbol == 'r' || symbol == 'R' || symbol == 't' || symbol == 'T' || symbol == 'y' || symbol == 'Y' || symbol == 'u' || symbol == 'U'){
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
